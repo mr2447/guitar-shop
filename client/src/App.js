bce5409f022d0cdb4bf5872aaac810e0bb91fb8f
Marvin Ren <marvin.ren@yale.edu>
2022-01-15 05:16:06 -0500

    client

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,23 @@
-import Header from "./components/Header";
-import Main from "./components/Main";
-import About from "./components/About";
-import Footer from "./components/Footer";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-    <Header />
-    <Main/>
-    <About />
-    <Footer />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
