23a1232f68ca70c9873e0ce6ad0b349a6594c097
Marvin Ren <marvin.ren@yale.edu>
2022-01-16 17:36:20 -0500

    add product form working

--- client/src/components/ProductForm/index.js
+++ client/src/components/ProductForm/index.js
@@ -1,54 +1,65 @@
 import React, { useState } from 'react'
-import { useQuery, useMutation } from '@apollo/client';
+import { useMutation } from '@apollo/client';
 import { ADD_PRODUCT } from '../../utils/mutations'
 import { QUERY_PRODUCTS, QUERY_ME } from '../../utils/queries';
 import EncodeBase64 from '../Image'
 
 const ProductForm = () => {
-    const [formState, setFormState] = useState({ brand: '', condition: '', price: ''});
-    const [addProduct, { error }] = useMutation(ADD_PRODUCT, {
-        update(cache, {data: { addProduct }}) {
-          try {
-            //could potentially not exist yet
-            const { products } = cache.readQuery({ query: QUERY_PRODUCTS})
-            cache.writeQuery({
-              query: QUERY_PRODUCTS,
-              data: { products: [addProduct, ...products]}
-            });
-          } catch (e) {
-            console.error(e)
-          }
+    const [formState, setFormState] = useState({ brand: '', price: '', condition: ''});
+    const { brand, price, condition } = formState;
+    const [addProduct, { error }] = useMutation(ADD_PRODUCT)
+    // ,{
+    //     update(cache, {data: { addProduct }}) {
+    //       try {
+    //         //could potentially not exist yet
+    //         const { products } = cache.readQuery({ query: QUERY_PRODUCTS})
+    //         cache.writeQuery({
+    //           query: QUERY_PRODUCTS,
+    //           data: { products: [addProduct, ...products]}
+    //         });
+    //       } catch (e) {
+    //         console.error(e)
+    //       }
   
-          // update me object's cache, appending new thought to the end of the array
-          const { me } = cache.readQuery({ query: QUERY_ME });
-          cache.writeQuery({
-            query: QUERY_ME,
-            data: { me: { ...me, products: [...me.products, addProduct] } }
-          });
-        }
-    });
-    let name
-    const handleChange = event => {
-        const { name: value } = event.target;
+    //       // update me object's cache, appending new thought to the end of the array
+    //       const { me } = cache.readQuery({ query: QUERY_ME });
+    //       cache.writeQuery({
+    //         query: QUERY_ME,
+    //         data: { me: { ...me, products: [...me.products, addProduct] } }
+    //       });
+    //     }
+    // });
 
-        setFormState({
-          ...formState, 
-          [name]: value
-        })
-        
+    const handleChange = (e) => {
+
+      setFormState({...formState, [e.target.name]: e.target.value})
+         
     };
-    console.log(formState);
+    console.log("input: ", formState);
 
     //submit form
     const handleFormSubmit = async (event) => {
         event.preventDefault();
+   
+        // console.log("typeof price", typeof parseInt(formState.price))
+        
         try {
-            //execute addUser mutation and pass in variable data from form
-            const { data } = await addProduct({
-                variables: { ...formState}
-            });
+          console.log("typeof brand",typeof formState.brand)
+          const { data } = await addProduct({
+            variables: {
+              brand: formState.brand,
+              price: parseInt(formState.price),
+              condition: formState.condition
+            }
+          });
+
+          console.log(data)
+            //execute addProduct mutation and pass in variable data from form
+            // const { data } = await addProduct({
+            //     variables: { ...formState, brand}
+            // });
             
-            console.log(data)
+            // console.log(data)
     
 
         } catch (e) {
@@ -65,33 +76,26 @@ const ProductForm = () => {
         <div>
             <form onSubmit={handleFormSubmit}>
               <input
-                className='form-input'
                 placeholder='Brand'
                 name='brand'
-                type='brand'
-                id='brand'
-                defaultValue={formState.brand}
+                type='text'
+                defaultValue={brand}
                 onChange={handleChange}
               ></input>
-              <input
-                className='form-input'
-                placeholder='condition'
-                name='condition'
-                type='condition'
-                id='condition'
-                defaultValue={formState.condition}
-                onChange={handleChange}
-              ></input>
-              <input
-                
-                className='form-input'
+              <input              
                 placeholder='price'
                 name='price'
                 type='price'
-                id='price'
-                defaultValue= {formState.price}
+                defaultValue= {price}
                 onChange={handleChange}
               ></input>
+                <input
+                placeholder='condition'
+                name='condition'
+                type='condition'
+                defaultValue={condition}
+                onChange={handleChange}
+                ></input>
               <button type='submit'>
                 Submit
               </button>
