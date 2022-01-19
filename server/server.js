7fcf04fff6ee9cee49b8c5b1f8a6f5a87ce7f8f3
Jorman <jorman.iisrael@me.com>
2022-01-19 15:41:12 -0500

    Deploy

--- server/server.js
+++ server/server.js
@@ -26,6 +26,10 @@ const startServer = async () => {
   //integrate our Apollo server with the Express application as middleware
   server.applyMiddleware({ app });
 
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
   //log where we can go to test our GQL API
   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
 };
