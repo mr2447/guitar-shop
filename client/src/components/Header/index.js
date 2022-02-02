1775c04bf950dd705873b0926ace36cf3c381802
Jorman <jorman.iisrael@me.com>
2022-02-02 12:08:22 -0500

    sd

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import './style.css'
 import { Link } from 'react-router-dom';
 import Auth from '../../utils/auth'
@@ -8,18 +8,7 @@ function Header(){
         Auth.logout();
     }
     
-    const [navSelected, setNavSelected]= useState(false);
-    const handleResize = () => {
-        if (window.innerWidth > 575) {
-            setNavSelected(true)
-        } else {
-            setNavSelected(false)
-        }
-    }
-
-    useEffect(() => {
-        window.addEventListener("resize", handleResize)
-      })
+   
 
   return(
       <section>
@@ -28,15 +17,15 @@ function Header(){
                     <h1><Link className='clear logo' to="/">Guitar Shop</Link></h1>
                     <div 
                     className='menu-img'
-                    onClick={()=> setNavSelected(true)}
+                    
                      >
                         <img src={`${process.env.PUBLIC_URL}/img/menu-icon.jpg`} alt='menu'/> 
                     </div>
               </div>
-             {navSelected && <div className='navigate'>
+             <div className='navigate'>
                     <div 
                     className='exit-btn'
-                    onClick={()=> setNavSelected(false)}
+                    
                         >
                         <img 
                         src={`${process.env.PUBLIC_URL}/img/exit.svg`} alt='menu'/>
@@ -45,17 +34,17 @@ function Header(){
                       <li><Link 
                       className='clear'  
                       to="/shop"
-                      onClick={()=> setNavSelected(false)}
+                      
                       >Shop</Link></li>
                       <li><Link 
                       className='clear' 
                       to="/about"
-                      onClick={()=> setNavSelected(false)}
+                      
                       >About</Link></li>
                       <li><Link 
                       className='clear' 
                       to="/contact"
-                      onClick={()=> setNavSelected(false)}
+                      
                       >Contact</Link></li>
                       {Auth.loggedIn() ? (
                           
@@ -74,17 +63,17 @@ function Header(){
                          <Link 
                          className='divide' 
                          to="/login"
-                         onClick={()=> setNavSelected(false)}>Login</Link>
+                         >Login</Link>
                           
                           <Link 
                           className='divide' 
                           to="/signup"
-                          onClick={()=> setNavSelected(false)}>Signup</Link>
+                          >Signup</Link>
                          </li>
                           
                       )}
                     </ul>
-              </div>}
+              </div>
             
 
           </div>
